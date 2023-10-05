c77d686d90360017cc195b1e48eec473bb1b9d43
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-10-05 23:19:54 +0300

    feat: added header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import Header from '@/components/Header'
-import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: 'Home | Next App',
@@ -20,7 +19,6 @@ export default function RootLayout({
       <main className="container">
         {children}
       </main>
-      <Footer />
       </body>
     </html>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,26 @@
+"use client";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Link from "next/link";
 
-export default function Header() {
+function Header() {
     return (
-        <header>
-            <Link href="/">Home</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/about">About</Link>
-        </header>
-    )
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand as={Link} href="/">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} href="/">Home</Nav.Link>
+                        <Nav.Link as={Link} href="/blog">Blog</Nav.Link>
+                        <Nav.Link as={Link} href="/about">About</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
+
+export default Header;
